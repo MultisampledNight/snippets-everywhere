ef80e1a27862f58f50ed7f9f1dd3c9a327136ec6
MultisampledNight <contact@multisamplednight.com>
2023-06-03 22:13:57 +0200

    Fix error formatting function to compile

--- src/backends/ultisnips/de.rs
+++ src/backends/ultisnips/de.rs
@@ -1,7 +1,7 @@
 use chumsky::prelude::*;
 use thiserror::Error;
 
-use crate::{SnippetFile, Snippet};
+use crate::SnippetFile;
 
 pub fn deserialize(input: &str) -> anyhow::Result<SnippetFile> {
     parser().parse(input).map_err(ParseError).map_err(Into::into)
@@ -10,10 +10,16 @@ pub fn deserialize(input: &str) -> anyhow::Result<SnippetFile> {
 #[derive(Debug, Error)]
 #[error(
     "error(s) parsing UltiSnips snippet file:\n{}",
-    .0.into_iter().map(|err| format!("{err}").chars()).flatten().collect::<String>()
+    .0.iter().flat_map(|err| {
+        err
+            .to_string()
+            .chars()
+            .collect::<Vec<_>>()
+            .into_iter()
+    }).collect::<String>()
 )]
 struct ParseError(Vec<Simple<char>>);
 
 fn parser() -> impl Parser<char, SnippetFile, Error = Simple<char>> {
-    todo!()
+    todo()
 }
