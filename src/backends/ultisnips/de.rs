1ea096542396d2bc71a1b3e9d633e7ec567565d9
MultisampledNight <contact@multisamplednight.com>
2023-06-27 08:49:41 +0200

    (semi-functional) Add basic ultisnips-in parsing

--- src/backends/ultisnips/de.rs
+++ src/backends/ultisnips/de.rs
@@ -3,15 +3,15 @@ use std::num::ParseIntError;
 use anyhow::Result;
 use itertools::Itertools;
 use thiserror::Error;
+use unicode_segmentation::UnicodeSegmentation;
 
 use crate::{Snippet, SnippetFile};
 
 pub fn deserialize(input: &str) -> Result<SnippetFile> {
-    // TODO: `priority n` commands
     // TODO: `extends` command, maybe not even necessary
 
     let mut snippets = Vec::new();
-    let mut current_priority = 0;
+    let mut current_priority = None;
 
     // external since it's also used inside the loop itself
     let mut lines_iter = input.lines().peekable();
@@ -38,7 +38,7 @@ pub fn deserialize(input: &str) -> Result<SnippetFile> {
                 let snippet = parse_snippet(&relevant_lines, current_priority)?;
                 snippets.push(snippet);
             }
-            Some("priority") => current_priority = parse_priority(line)?,
+            Some("priority") => current_priority = Some(parse_priority(line)?),
             Some(unknown) => {
                 return Err(ParseError::UnknownDirective {
                     directive: unknown.to_string(),
@@ -55,14 +55,73 @@ pub fn deserialize(input: &str) -> Result<SnippetFile> {
 enum ParseError {
     #[error("unknown directive: `{directive}`")]
     UnknownDirective { directive: String },
-    #[error("no number after `priority` directive")]
+    #[error("found no trigger after `snippet`")]
+    MissingSnippetTrigger,
+    #[error("found no number after `priority` directive")]
     MissingPriorityNumber,
     #[error("tried to parse number in `{subject}` but failed: {err}")]
     ParsePriorityNumber { subject: String, err: ParseIntError },
 }
 
-fn parse_snippet(lines: &[String], current_priority: i64) -> Result<Snippet> {
-    todo!()
+fn parse_snippet(lines: &[String], priority: Option<i64>) -> Result<Snippet, ParseError> {
+    // basically snippet/source/file/ulti_snips.py in the UltiSnips repo ported
+    // first parsing the signature `snippet trigger [ description [ options ] ]`
+    // remember: description and options are optional, trigger may be quoted weirdly
+    let first_line = lines.get(0).expect("caller passing lines to parse");
+    let mut parts: Vec<_> = first_line.split_whitespace().collect();
+
+    let trigger;
+    let mut description = None;
+    let mut options = None;
+
+    match parts.len() {
+        0 => panic!("expected caller to not pass empty lines"),
+        1 => return Err(ParseError::MissingSnippetTrigger),
+        2 => {
+            // `snippet trigger` (being unquoted)
+            trigger = parts.last().unwrap().to_string();
+        }
+        _ => {
+            // possibly description,
+            // possibly description with option,
+            // possibly just quoted trigger
+
+            // are options there?
+            if !parts.last().unwrap().ends_with('"') && parts[parts.len() - 2].ends_with('"') {
+                options = parts.pop().map(|opts| opts.to_string());
+            }
+
+            // is a description there?
+            if parts.last().unwrap().ends_with('"') {
+                let _accumulated_desc = String::new();
+                todo!()
+            }
+
+            // then everything remaining will be the trigger
+            if parts.len() >= 2 {
+                // quoted
+                // actually according to :h UltiSnips-snippet-options, both single-word and
+                // multi-word triggers can be quoted, but the code only implements the latter
+                // (and checks for regex, too). so that's emulated here
+                let quoted = parts[1..].iter().format(" ").to_string();
+                let graphemes: Vec<_> = quoted.graphemes(true).collect();
+                trigger = graphemes[1..graphemes.len() - 1].iter().copied().collect();
+            } else {
+                // unquoted
+                trigger = parts[1].to_string();
+            }
+        }
+    }
+
+    let replacement = lines[1..lines.len() - 1].iter().format("\n").to_string();
+
+    Ok(Snippet {
+        trigger,
+        replacement,
+        options,
+        description,
+        priority,
+    })
 }
 
 fn parse_priority(line: &str) -> Result<i64, ParseError> {
